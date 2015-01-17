76897936c6ef51ba7d969c10826f08904ca403cc
Hein Bekker <hein@netbek.co.za>
2015-01-17 21:18:48 +0200

    Add defaultsDeep and equalForKeys

--- src/js/nb-lodash.js
+++ src/js/nb-lodash.js
@@ -14,6 +14,43 @@
 		.factory('_', ['$window', function _ ($window) {
 				var _ = $window._;
 				delete $window._;
+
+				/**
+				 * Assigns own enumerable properties of source object(s) to the
+				 * destination object for all destination properties that
+				 * resolve to undefined. Once a property is set, additional
+				 * defaults of the same property will be ignored.
+				 *
+				 * Returns a function.
+				 */
+				_.defaultsDeep = _.partialRight(_.merge, function deep (value, other) {
+					return _.merge(value, other, deep);
+				});
+
+				/**
+				 * Performs a non-strict comparison of the subset of two objects, defined by a list of keys.
+				 *
+				 * @param {Object} a The first object.
+				 * @param {Object} b The second object.
+				 * @param {Array} keys The list of keys within each object to compare. If the list is empty or not specified,
+				 *                     it defaults to the list of keys in `a`.
+				 * @return {Boolean} Returns `true` if the keys match, otherwise `false`.
+				 */
+				_.equalForKeys = function (a, b, keys) {
+					if (!keys) {
+						keys = [];
+						for (var n in a)
+							keys.push(n); // Used instead of Object.keys() for IE8 compatibility
+					}
+
+					for (var i = 0; i < keys.length; i++) {
+						var k = keys[i];
+						if (a[k] != b[k])
+							return false; // Not '===', values aren't necessarily normalized
+					}
+					return true;
+				};
+
 				return _;
 			}])
 		.run(runBlock);
